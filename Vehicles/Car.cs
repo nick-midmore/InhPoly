2400fda882e3be7f7a753e55ad001a78076829a7
Nick Midmore <nmidmore@gmail.com>
2024-11-12 11:32:19 +0000

    Add Accelerate logic, tasks 9-12

--- Vehicles/Car.cs
+++ Vehicles/Car.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Vehicles.Engine;
 
 namespace Vehicles
 {
@@ -11,6 +12,22 @@ namespace Vehicles
         public Car(string make, string model) : base(make, model, new CarEngine(150, Engine.fuelType.Diesel))
         {
            
+        }
+
+        protected override void Accelerate()
+        {
+            string message = $"{Make} {Model} speed: ";
+            double fuelMod = EngineType.FuelType switch
+            {
+                fuelType.Unleaded => 1.5D,
+                fuelType.Leaded => 1.2D,
+                fuelType.Diesel => 1.8D,
+                _ => throw new InvalidOperationException($"Unexpected value: {EngineType.FuelType}")
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{message}{((EngineType.Horsepower * fuelMod) * Math.Exp(Math.Sqrt(i)) / 2):F2}mph");
+            }
         }
     }
 }
--- Vehicles/Motorcycle.cs
+++ Vehicles/Motorcycle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Vehicles.Engine;
 
 namespace Vehicles
 {
@@ -15,5 +16,20 @@ namespace Vehicles
             this.HasSidecar = hasSideCar;
         }
 
+        protected override void Accelerate()
+        {
+            string message = $"{Make} {Model} speed: ";
+            double fuelMod = EngineType.FuelType switch
+            {
+                fuelType.Unleaded => 1.5D,
+                fuelType.Leaded => 1.2D,
+                fuelType.Diesel => 1.8D,
+                _ => throw new InvalidOperationException($"Unexpected value: {EngineType.FuelType}")
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{message}{((EngineType.Horsepower * fuelMod) * Math.Exp(Math.Sqrt(i)) / 2) * (HasSidecar ? 0.8 : 1.0)}mph");
+            }
+        }
     }
 }
