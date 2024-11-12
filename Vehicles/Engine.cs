581946cbd4975a2fc141604b9aed7c14c24e64c8
Nick Midmore <nmidmore@gmail.com>
2024-11-12 10:44:01 +0000

    adds up to Task 4

---
+++ Vehicles/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    public abstract class Engine
+    {
+        public bool Running { get; set; }
+        public int Horsepower { get; set; }
+        public enum fuelType { Unleaded, Leaded, Diesel };
+        public fuelType FuelType { get; set; }
+
+        public Engine(int horsepower, fuelType fueltype)
+        {
+            Horsepower = horsepower;
+            FuelType = fueltype;
+        }
+    }
+}
